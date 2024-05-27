0a6140d3e93ed50f62482a0ea8dc5ab59713cfd9
Nilesh Mistry <nileshmistry@192.168.1.3>
2024-05-27 14:58:17 +0800

    Added support of cognito user pool

--- mergeSchema.js
+++ mergeSchema.js
@@ -16,6 +16,7 @@ const awsScalars = [
   'scalar AWSIPAddress',
   'directive @aws_subscribe(mutations: [String!]!) on FIELD_DEFINITION',
   'directive @aws_api_key on OBJECT | FIELD_DEFINITION',
+  'directive @aws_cognito_user_pools on OBJECT | FIELD_DEFINITION',
 ];
 const mergedSchemaFileName = 'schema.graphql';
 if (fs.existsSync(mergedSchemaFileName)) {
